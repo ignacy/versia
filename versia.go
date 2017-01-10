5eda1bce48f0cb9fee568dec824e399cffa719e9
Ignacy Moryc <imoryc@gmail.com>
2017-01-10 20:47:26 +0100

    Prints versions

--- versia.go
+++ versia.go
@@ -8,9 +8,14 @@ import (
 )
 
 var (
-	amount string
-	id     int
+	event          string
+	whodunnit      string
+	object         string
+	object_changes string
+	id             int
 )
+
+// 29
 
 func main() {
 	db, err := sql.Open("postgres", "dbname=advanon_development sslmode=disable")
@@ -19,17 +24,26 @@ func main() {
 	}
 	defer db.Close()
 
-	rows, err := db.Query("SELECT id, amount FROM invoices WHERE status = $1", 1)
+	rows, err := db.Query(`
+       SELECT
+           id,
+           COALESCE(event, '') as event,
+           COALESCE(whodunnit, '') as whodunnit,
+           COALESCE(object, '') as object,
+           COALESCE(object_changes, '') as object_changes
+       FROM versions WHERE item_type = 'Invoice' AND item_id = $1
+      `, 29)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rows.Close()
 	for rows.Next() {
-		err := rows.Scan(&id, &amount)
+		err := rows.Scan(&id, &event, &whodunnit, &object, &object_changes)
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Println(id, amount)
+		log.Println(id, event, whodunnit, object, object_changes)
 	}
 	err = rows.Err()
 	if err != nil {
