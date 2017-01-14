11f0f2fff3a25064a74123c5e815badc0b5899cf
Ignacy Moryc <imoryc@gmail.com>
2017-01-14 14:58:19 +0100

    Renders nicely formatted views

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"crypto/subtle"
-	"fmt"
 	"net/http"
 	"strconv"
 
+	"github.com/alecthomas/template"
 	"github.com/ignacy/versia/storage"
 	_ "github.com/lib/pq"
 )
@@ -15,22 +15,33 @@ func invoiceHandler(w http.ResponseWriter, r *http.Request) {
 	i, _ := strconv.Atoi(id)
 	versions := storage.FindVersions(i)
 
-	fmt.Fprintf(w, "<h1>%s</h1>", id)
-	for _, v := range versions {
-		fmt.Fprintf(w, "<h4>%s %s %s</h4>", v.Event, v.Id, v.Whodunnit)
-		fmt.Fprintf(w, "<div>%s</div>", v.Object)
-		fmt.Fprintf(w, "<div>%s</div>", v.Object_changes)
+	t, err := template.ParseFiles("versions.html")
+	if err != nil {
+		panic(err)
+	}
+	err = t.Execute(w, versions)
+	if err != nil {
+		panic(err)
 	}
 }
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello there!</h1>")
+	t, err := template.ParseFiles("index.html")
+	if err != nil {
+		panic(err)
+	}
+	err = t.Execute(w, nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", BasicAuth(handleIndex, "admin", "123456", "Auth"))
-	http.HandleFunc("/invoice/", BasicAuth(invoiceHandler, "admin", "123456", "Auth"))
-	http.ListenAndServe(":8080", nil)
+	mux := http.NewServeMux()
+	mux.Handle("/", http.HandlerFunc(BasicAuth(handleIndex, "admin", "123456", "Auth")))
+	mux.Handle("/invoice/", http.HandlerFunc(BasicAuth(invoiceHandler, "admin", "123456", "Auth")))
+	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./static-assets"))))
+	http.ListenAndServe(":8080", mux)
 }
 
 func BasicAuth(handler http.HandlerFunc, username, password, realm string) http.HandlerFunc {
